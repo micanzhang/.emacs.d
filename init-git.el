dc5276eb84308a7b892aab99813fa48842c9c93e
unknown <micanzhang@micanzhang.synacast.local>
2014-07-07 16:28:09 +0800

    fix cannot load file "magit-svn" err.and add go mode support for go language

--- init-git.el
+++ init-git.el
@@ -33,53 +33,6 @@
   (define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
 
 
-;;; When we start working on git-backed files, use git-wip if available
-
-(after-load 'vc-git
-  (global-magit-wip-save-mode)
-  (diminish 'magit-wip-save-mode))
-
-
-;;; Use the fringe version of git-gutter
-
-(after-load 'git-gutter
-  (require 'git-gutter-fringe))
-
-
-;; Convenient binding for vc-git-grep
-(global-set-key (kbd "C-x v f") 'vc-git-grep)
-
-
-
-;;; git-svn support
-
-(after-load 'magit-key-mode
-  (require 'magit-svn))
-
-(after-load 'compile
-  (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
-                      '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
-    (add-to-list 'compilation-error-regexp-alist-alist defn)
-    (add-to-list 'compilation-error-regexp-alist (car defn))))
-
-(defvar git-svn--available-commands nil "Cached list of git svn subcommands")
-
-(defun git-svn (dir)
-  "Run a git svn subcommand in DIR."
-  (interactive "DSelect directory: ")
-  (unless git-svn--available-commands
-    (setq git-svn--available-commands
-          (string-all-matches "^  \\([a-z\\-]+\\) +" (shell-command-to-string "git svn help") 1)))
-  (let* ((default-directory (vc-git-root dir))
-         (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
-    (compile (concat "git svn "
-                     (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
-
-
-(require-package 'git-messenger)
-(global-set-key (kbd "C-x v p") #'git-messenger:popup-message)
-
-
 ;;; github
 
 (require-package 'yagist)
