0079c50c308981a881404129bee95794b64bb278
micanzhang <micanzhang@gmail.com>
2015-09-09 18:13:18 +0800

    custom default font

--- lisp/custom.el
+++ lisp/custom.el
@@ -12,5 +12,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(setq exec-path-from-shell-variables '("GOHOME" "MYGOHOME" "QBASE" "QPORTAL" "QADMINPATH" "QPORTALPATH" "QBIZPATH"))
