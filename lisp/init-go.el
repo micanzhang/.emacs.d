b013c2c7f71014adbde198a606c9abf121f9353f
micanzhang <micanzhang@gmail.com>
2015-11-18 14:16:06 +0800

    bugfix of set GOPATH env on macos

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -9,6 +9,9 @@
 (require 'auto-complete-config)
 (ac-config-default)
 
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH"))
 ;; for windows user 
 ;(defun set-exec-path-from-shell-PATH ()
 ;  (let ((path-from-shell (replace-regexp-in-string
