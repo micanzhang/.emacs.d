cbdf7d06a1982febdbdbf6c306aa649d3ca65e9a
micanzhang <micanzhang@gmail.com>
2015-11-18 12:12:24 +0800

    ignore session;update golang config

--- init-go.el
+++ init-go.el
@@ -1,49 +1,32 @@
 ;;;init mode for google golang code which
 ;;;depend on http://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/
 
+;; go-mode 
 (require-package 'go-mode)
 
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (replace-regexp-in-string
-                          "[ \t\n]*$"
-                          ""
-                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq eshell-path-env path-from-shell) ; for eshell users
-    (setq exec-path (split-string path-from-shell path-separator))))
+;; go-autocomplete
+(require-package 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
 
-(when window-system (set-exec-path-from-shell-PATH))
+;; for windows user 
+;(defun set-exec-path-from-shell-PATH ()
+;  (let ((path-from-shell (replace-regexp-in-string
+;                          "[ \t\n]*$"
+;                          ""
+;                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+;    (setenv "PATH" path-from-shell)
+;    (setq eshell-path-env path-from-shell) ; for eshell users
+;    (setq exec-path (split-string path-from-shell path-separator))))
+;
+;(when window-system (set-exec-path-from-shell-PATH))
 
-(setenv "GOPATH" "/home/micanzhang/Code/golang")
-
-(setq exec-path (cons "/usr/local/go/bin" exec-path))
-(add-to-list 'exec-path "/home/micanzhang/Code/golang/bin")
-(add-hook 'before-save-hook 'gofmt-before-save)
-
+;;make sure run ~$go get golang.org/x/tools/cmd/oracle
+;;load oracle.el
 (load-file "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
-
-(defun my-go-mode-hook ()
-  ; Godef jump key binding
-  (local-set-key (kbd "F5") 'go-oracle-callers)
-  (local-set-key (kbd "F6") 'go-oracle-callees)
-  )
-
-(add-hook 'go-mode-hook 'my-go-mode-hook)
-
 
 ;;firstly, please esure godef existed,if not, run go get -v github.com/rogpeppe/godef
 ;;for some reason,you cannot run godef at emacs, a way to fix that is make a soft link to binary of godef
-(defun my-go-mode-hook ()
-  ; Call Gofmt before saving
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  ; Godef jump key binding
-  (local-set-key (kbd "M-.") 'godef-jump))
-(add-hook 'go-mode-hook 'my-go-mode-hook)
-
-(load-file "$GOPATH/src/github.com/nsf/gocode/emacs/go-autocomplete.el")
-;;(require 'go-autocomplete)
-(require 'auto-complete-config)
-(ac-config-default)
 
 (defun my-go-mode-hook ()
   ;Call Gofmt before saving
@@ -55,7 +38,7 @@
   ; Godef jump key binding
   (local-set-key (kbd "C-x C-g") 'godef-jump)
   (local-set-key (kbd "C-x M-g") 'godef-jump-other-window)
-(local-set-key (kbd "M-.") 'godef-jump))
+  )
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (provide 'init-go)
