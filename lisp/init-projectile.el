b1fc09e02ca0bcddfd6f44c8d3eb79f4f34bacbc
micanzhang <micanzhang@gmail.com>
2016-11-29 23:50:07 +0800

    customize projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,6 +1,15 @@
 (when (maybe-require-package 'projectile)
   (add-hook 'after-init-hook 'projectile-global-mode)
 
+  ;; use os native index method for best performance
+  (setq projectile-indexing-method 'native)
+
+  ;; jump to root directory of project instead of open project file
+  (setq projectile-switch-project-action 'projectile-dired)
+
+  ;; set grizzl as default completion option
+  (setq projectile-completion-system 'grizzl)
+
   ;; The following code means you get a menu if you hit "C-c p" and wait
   (after-load 'guide-key
     (add-to-list 'guide-key/guide-key-sequence "C-c p"))
@@ -13,7 +22,6 @@
        (if (file-remote-p default-directory)
            " Pr"
          (format " Pr[%s]" (projectile-project-name)))))))
-
 
 
 (provide 'init-projectile)
