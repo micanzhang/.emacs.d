84deabf75931f28b4a1ac3144cc7c8942bdf1589
micanzhang <micanzhang@gmail.com>
2017-02-16 11:44:31 +0800

    fix helm-gtags set keybinding bug

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -5,12 +5,13 @@
 (require-package 'helm-ag)
 (require-package 'helm-gtags)
 
-(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+(with-eval-after-load 'helm-gtags
+  (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+  (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+  (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+  (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+  (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history))
 
 (global-set-key (kbd "M-x")  #'helm-M-x)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
