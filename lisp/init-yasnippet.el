94723af2f933e99fe878c07faa061b22f01eb100
micanzhang <micanzhang@gmail.com>
2017-02-22 23:43:35 +0800

    change yas snippets directory

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -3,7 +3,7 @@
 ;; customize snippet dirs
 (let ((snippets-dir "~/Documents/keep/snippets"))
   (when (file-exists-p snippets-dir)
-    (setq yas-snippet-dirs '(snippets-dir yas-installed-snippets-dir))))
+    (setq yas-snippet-dirs '("~/Documents/keep/snippets" yas-installed-snippets-dir))))
 
 ;; enable global mode
 (yas-global-mode 1)
