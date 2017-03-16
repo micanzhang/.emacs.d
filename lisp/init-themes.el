b51b9cedf078fc622f32c4b7c028a3a81ce5f35f
Mican Zhang <micanzhang@gmail.com>
2017-03-16 12:10:28 +0800

    set material light as default theme.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -14,7 +14,7 @@
 (load-theme 'me t t)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-night))
+(setq-default custom-enabled-themes '(material-light))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
@@ -25,7 +25,6 @@
   (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
 
 (add-hook 'after-init-hook 'reapply-themes)
-
 
 ;;------------------------------------------------------------------------------
 ;; Toggle between light and dark
