fa3a26e5fdb1a6de894022bd35d90a67b8cfc8b6
Mican Zhang <mican.zhang@liulishuo.com>
2020-09-21 16:42:01 +0800

    remove custom snippets directory

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,11 +1,6 @@
 (require-package 'yasnippet)
 (require-package 'yasnippet-snippets)
 
-;; customize snippet dirs
-(let ((snippets-dir "~/Google Drive/keep/snippets"))
-  (when (file-exists-p snippets-dir)
-    (setq yas-snippet-dirs '("~/Google Drive/keep/snippets" yasnippet-snippets-dir))))
-
 ;; enable global mode
 (yas-global-mode 1)
 
